c2a329ec9b272fcfdb2d49499c8102d186b7c2e5
Nirvana16 <zedanilo161@gmail.com>
2016-12-02 16:41:37 -0200

    Realizado esboco do layout principal

--- app/src/main/java/br/edu/iff/pooa20162/ajude/MainActivity.java
+++ app/src/main/java/br/edu/iff/pooa20162/ajude/MainActivity.java
@@ -1,20 +1,17 @@
 package br.edu.iff.pooa20162.ajude;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class AlertaActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_alerta);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -23,7 +20,7 @@ public class AlertaActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_alerta, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
