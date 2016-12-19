84cd08ee4e08962e9f6571b731da6806f40bdcb8
Nirvana16 <zedanilo161@gmail.com>
2016-12-19 11:03:04 -0200

    Atualizado ReadMe e classe de servico

--- app/src/main/java/br/edu/iff/pooa20162/ajude/services/service.java
+++ app/src/main/java/br/edu/iff/pooa20162/ajude/services/service.java
@@ -7,13 +7,13 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class service extends Service{
-    private static final String TAG = "MyService";
+    private static final String TAG = "AjudeService";
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
     public void onDestroy() {
-        Toast.makeText(this, "My Service Stopped", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "O servico Ajude parou", Toast.LENGTH_LONG).show();
         Log.d(TAG, "onDestroy");
     }
 
@@ -23,7 +23,7 @@ public class service extends Service{
         Intent intents = new Intent(getBaseContext(),SMSReceiver.class);
         intents.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intents);
-        Toast.makeText(this, "My Service Started", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "O Servico Ajude foi iniciado", Toast.LENGTH_LONG).show();
         Log.d(TAG, "onStart");
     }
 }
