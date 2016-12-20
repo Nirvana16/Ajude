59fc57e5845a93e8310ba06c0ec74e6a4d39bc10
Nirvana16 <zedanilo161@gmail.com>
2016-12-20 13:22:28 -0200

    Primeira implementacao para p2

--- app/src/main/java/br/edu/iff/pooa20162/ajude/model/Contato.java
+++ app/src/main/java/br/edu/iff/pooa20162/ajude/model/Contato.java
@@ -5,4 +5,32 @@ package br.edu.iff.pooa20162.ajude.model;
  */
 
 public class Contato {
+
+    private String nome;
+    private String telefone;
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Contato(){
+        nome = "Fulando de tal";
+        telefone = "+5522997814310";
+    }
+
+
+
+
 }
--- app/src/main/java/br/edu/iff/pooa20162/ajude/model/Mensagem.java
+++ app/src/main/java/br/edu/iff/pooa20162/ajude/model/Mensagem.java
@@ -1,5 +1,7 @@
 package br.edu.iff.pooa20162.ajude.model;
 
+import android.telephony.SmsManager;
+
 /**
  * Created by Nirvana161 on 19/12/16.
  */
@@ -9,4 +11,20 @@ public class Mensagem {
     private String conteudo = "\nPRECISO DE AJUDA \n Estou em: LOCALIZACAO";
 
 
+    public void EnviarSMS() {
+
+        Contato objContato = new Contato();
+        /*
+           SmsManager, API que faz toda a magica acontecer.
+           lembrar de ler a documentação mais a fundo
+           https://developer.android.com/reference/android/telephony/SmsManager.html
+         */
+        SmsManager smsManager = SmsManager.getDefault();
+        // O for serve para enviar multiplas mensagens, haverá um parametro na tela de configurações
+        // Onde o usuário poderá definir quantas mensagens ele quer enviar. Por ora coloquei 4.
+
+        for (int i = 0; i < 4; i++) {
+            smsManager.sendTextMessage(objContato.getTelefone(), null, "ATENCAO! " + objContato.getNome() + "\n" + conteudo, null, null);
+        }
+    }
 }
