33e9985de346a2c96191b1491a97cc3345808fe4
Nirvana16 <zedanilo161@gmail.com>
2016-12-21 11:07:44 -0200

    Corrigido o bug das permissões Solicitando ao usuario que forneca as mesmas na SplashScreen, ainda falta implementar melhor

--- app/src/main/java/br/edu/iff/pooa20162/ajude/AberturaActivity.java
+++ app/src/main/java/br/edu/iff/pooa20162/ajude/AberturaActivity.java
@@ -1,9 +1,15 @@
 package br.edu.iff.pooa20162.ajude;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import static android.Manifest.permission.RECEIVE_SMS;
 
 public class AberturaActivity extends AppCompatActivity implements Runnable {
 
@@ -12,8 +18,29 @@ public class AberturaActivity extends AppCompatActivity implements Runnable {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abertura);
 
-        Handler handler = new Handler();
-        handler.postDelayed(this, 2000);
+        ActivityCompat.requestPermissions(AberturaActivity.this,new String[]{Manifest.permission.RECEIVE_BOOT_COMPLETED},0);
+
+        // Se não possui permissão
+        if (ContextCompat.checkSelfPermission(AberturaActivity.this,RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+
+           Handler handler = new Handler();
+           handler.postDelayed(this, 5000);
+
+            // Solicita a permissão 1
+            ActivityCompat.requestPermissions(AberturaActivity.this,new String[]{RECEIVE_SMS},0);
+
+            // Verifica se já mostramos o alerta e o usuário negou na 1ª vez.
+            if (ActivityCompat.shouldShowRequestPermissionRationale(AberturaActivity.this,Manifest.permission.RECEIVE_SMS)) {
+                // Caso o usuário tenha negado a permissão anteriormente, e não tenha marcado o check "nunca mais mostre este alerta"
+                // Podemos mostrar um alerta explicando para o usuário porque a permissão é importante.
+            } else {
+                // Solicita a permissão
+                ActivityCompat.requestPermissions(AberturaActivity.this,new String[]{Manifest.permission.RECEIVE_SMS},0);
+            }
+        } else {
+            Handler handler = new Handler();
+            handler.postDelayed(this, 1000);
+        }
     }
 
     public void run(){
